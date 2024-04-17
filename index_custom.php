c3a6ad8e855b2b9c13c580647a15895c1615be05
Shadraze <prabeshgiri@gmail.com>
2024-04-17 13:55:38 +0545

    update changelog

--- index_custom.php
+++ index_custom.php
@@ -1,4 +1,11 @@
 <?php
-echo "<h2>PHP is Fun!</h2>";
+//// NEPALI INTEGRATION
+// Hamro Patro Calender Toolbar
+// $homepage = file_get_contents('NepaliIntegration/hamroPatroCalenderToolbar.html');
+// echo $homepage;
+
+//// Test
+// echo "<h2>PHP is Fun!</h2>";
 ?>
-<h2>PHP is NOT Fun!</h2>
+<!-- Test -->
+<!-- <h2>PHP is NOT Fun!</h2> -->
