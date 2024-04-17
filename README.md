e6ebd9ae7ca5e9bd0ee6828971a96e8997de0aff
Shadraze <prabeshgiri@gmail.com>
2024-04-17 13:25:39 +0545

    merge with gibbon files for straightforward install

--- README.md
+++ README.md
@@ -1,17 +1,44 @@
-# WIP, not suited for production yet!!! only for testing!!!
+<p align="center">
+    <a href="https://gibbonedu.org/" target="_blank"><img width="200" src="https://gibbonedu.org/img/gibbon-logo.png"></a><br>
+    Gibbon is a flexible, open source school management platform designed <br>
+    to make life better for teachers, students, parents and schools.
+</p>
 
-# How-to:
-1. Copy paste the <b>inner contents</b> of the folder 'NepaliIntegration_GibbonFiles' into gibbon's main folder
+------
 
-2. Make sure docker is installed, if yes then skip this step, <br>
-otherwise run : 'apt install docker-compose-v2' (if necessary run, 'apt update' first)
+# WIP Nepali Version<br><br>
 
-3. To run the date helper backend: 'docker compose up --build -d',  (while in this directory) <br>
+Gibbon Core
+===========
+The Core repository represents the bulk of Gibbon, including all of its primary functionality. The core can be extended through the use of modules and themes, which are provided separately. See the [Extend](https://gibbonedu.org/extend/) page for more info.
 
-4. Refresh gibbon webpage
+Gibbon is open source, and maintained for the benefit of teachers, students, parents and schools.
 
-Notes:<br>
-i. To stop backend helper: 'docker compose down', (while in this directory)
+## Documentation
 
-ii. To check if it is running, run 'docker compose ls', (from any directory) <br>
-and check if 'nepali-date-helper' service is displayed and is also running.
+For full documentation, visit [docs.gibbonedu.org](https://docs.gibbonedu.org/).
+
+## Installation & Support
+
+For installation instructions, visit [Getting Started: Installing Gibbon](https://docs.gibbonedu.org/administrators/getting-started/installing-gibbon/)
+
+For support visit [ask.gibbonedu.org](https://ask.gibbonedu.org/) or see [our documentation](https://docs.gibbonedu.org/).
+
+## Cutting Edge
+If you want to run the latest version of Gibbon, prerelease, you can get the source from our [GitHub repository](https://github.com/GibbonEdu/core). Remember, though, it is not stable, and you may lose data. This is not for the faint of heart.
+
+For installation instructions, visit [Getting Started: Installing Gibbon](https://docs.gibbonedu.org/administrators/getting-started/installing-gibbon/), and make sure to follow the additional instructions for [Cutting Edge Code](https://docs.gibbonedu.org/administrators/getting-started/installing-gibbon/cutting-edge-code/).
+
+## Contributing
+
+We welcome community contribution and aim to ensure Gibbon is an open and friendly environment. Information about contributing, submitting issues, and pull requests can be found in the following docs:
+
+- [**Contributor Guide**](https://github.com/GibbonEdu/core/blob/master/.github/CONTRIBUTING.md) - Learn more about how you can contribute to Gibbon, from code to non-code contributions alike.
+
+- [**Code of Conduct**](https://github.com/GibbonEdu/core/blob/master/.github/CODE_OF_CONDUCT.md) - Our pledge to foster a welcoming community and a positive environment for anyone to participate in.
+
+- [**Developer Workflow**](https://docs.gibbonedu.org/developers/getting-started/developer-workflow/) - If you want to get involved in the development process, check out our workflow and [GitHub repository](https://github.com/GibbonEdu/core). Generally there will be a development branch with the latest code, as per our [Development Road Map](https://docs.gibbonedu.org/developers/getting-started/gibbon-road-map/).
+
+## License
+
+Gibbon is licensed under GNU General Public License v3.0. You can obtain a copy of the license [here](https://github.com/GibbonEdu/core/blob/master/LICENSE).
---
+++ modules/Messenger/messenger_postProcess.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Data\Validator;
+use Gibbon\Services\Format;
+use Gibbon\Module\Messenger\MessageProcess;
+use Gibbon\Module\Messenger\MessageTargets;
+use Gibbon\Domain\Messenger\MessengerGateway;
+
+require_once '../../gibbon.php';
+
+$_POST = $container->get(Validator::class)->sanitize($_POST, ['body' => 'HTML']);
+
+$address = $_POST['address'] ?? '';
+$URL = $session->get('absoluteURL') . '/index.php?q=/modules/Messenger/messenger_manage_post.php&sidebar=true';
+$URLSend = $session->get('absoluteURL') . '/index.php?q=/modules/Messenger/messenger_send.php&sidebar=true';
+$URLEdit = $session->get('absoluteURL') . '/index.php?q=/modules/Messenger/messenger_manage_edit.php&sidebar=true';
+
+if (isActionAccessible($guid, $connection2, "/modules/Messenger/messenger_post.php") == false) {
+    $URL .= "&return=error0";
+    header("Location: {$URL}");
+    exit;
+} else {
+    $messengerGateway = $container->get(MessengerGateway::class);
+    $messageTargets = $container->get(MessageTargets::class);
+
+    $sendTestEmail = $_POST['sendTestEmail'] ?? 'N';
+    $saveMode = $_POST['saveMode'] ?? 'Preview';
+    $data = [
+        'gibbonSchoolYearID'=> $gibbon->session->get('gibbonSchoolYearID'),
+        'status'            => $_POST['status'] ?? 'Draft',
+        'email'             => $_POST['email'] ?? 'N',
+        'messageWall'       => $_POST['messageWall'] ?? 'N',
+        'messageWallPin'    => $_POST['messageWallPin'] ?? 'N',
+        'messageWall_date1' => !empty($_POST['date1']) ? Format::dateConvert($_POST['date1']) : null,
+        'messageWall_date2' => !empty($_POST['date2']) ? Format::dateConvert($_POST['date2']) : null,
+        'messageWall_date3' => !empty($_POST['date3']) ? Format::dateConvert($_POST['date3']) : null,
+        'sms'               => $_POST['sms'] ?? 'N',
+        'subject'           => $_POST['subject'] ?? '',
+        'body'              => $_POST['body'] ?? '',
+        'emailFrom'         => $_POST['emailFrom'] ?? $session->get('email'),
+        'emailReplyTo'      => $_POST['emailReplyTo'] ?? $session->get('email'),
+        'emailReceipt'      => $_POST['emailReceipt'] ?? 'N',
+        'emailReceiptText'  => $_POST['emailReceiptText'] ?? '',
+        'individualNaming'  => $_POST['individualNaming'] ?? 'N',
+        'includeSignature'  => $_POST['includeSignature'] ?? 'N',
+        'confidential'      => $_POST['confidential'] ?? 'N',
+        'gibbonPersonID'    => $session->get('gibbonPersonID'),
+        'timestamp'         => date('Y-m-d H:i:s'),
+    ];
+
+    // Validate that the required values are present
+    if (empty($data['subject']) || empty($data['body']) || ($data['email'] == 'Y' && $data['emailFrom'] == '') || ($data['emailReceipt'] == 'Y' && $data['emailReceiptText'] == '')) {
+        $URL .= "&return=error3";
+        header("Location: {$URL}");
+        exit;
+    }
+
+    // Check for empty POST. This can happen if attachments go horribly wrong.
+    if (empty($_POST)) {
+        $URL .= "&return=error5";
+        header("Location: {$URL}");
+        exit;
+    }
+
+    // Insert the message and get the ID
+    $gibbonMessengerID = $messengerGateway->insert($data);
+    
+    $URLEdit .= "&gibbonMessengerID={$gibbonMessengerID}";
+    $URLSend .= "&gibbonMessengerID={$gibbonMessengerID}";
+
+    // Send Draft
+    $testEmail = 0;
+    if ($sendTestEmail == 'Y') {
+        $process = $container->get(MessageProcess::class);
+        $testEmail =  $process->runSendDraft($data);
+        $URLSend .= "&testEmail={$testEmail}";
+        $URLEdit .= "&testEmail={$testEmail}";
+    }
+
+    // Go to preview page?
+    if ($saveMode == 'Preview' && ($data['email'] == 'Y' || $data['sms'] == 'Y')) {
+        $recipients = $messageTargets->createMessageRecipientsFromTargets($gibbonMessengerID, $data);
+
+        if (empty($recipients)) {
+            $URLEdit.="&return=error6";
+            header("Location: {$URLEdit}");
+            exit;
+        }
+
+        header("Location: {$URLSend}");
+        exit;
+    } elseif ($saveMode == 'Preview' && $data['messageWall'] == 'Y') {
+        $messengerGateway->update($gibbonMessengerID, ['status' => 'Sent']);
+    }
+
+    // Otherwise save any edits to targets
+    $messageTargets->createMessageTargets($gibbonMessengerID, $partialFail);
+
+    $URLEdit .= $partialFail
+        ? "&return=warning1"
+        : "&return=success0";
+
+    header("Location: {$URLEdit}") ;
+}
